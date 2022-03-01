48704735d18fb75528c85c242a66bd119f1dd89f
LEE <vine127@gmail.com>
2022-03-01 14:27:19 +0900

    feat Q&A페이지

---
+++ assets/data/QAdata.js
@@ -0,0 +1,39 @@
+var QAdata = [
+  {
+    id: 0,
+    title: 'Q&A1',
+    name: '관리자',
+    question: '이거는 어떻게 하나요?1',
+    date: '0000. 00. 00.',
+    answer: '이렇게 합니다1',
+    url: '/QApage/QApage/0',
+  },
+  {
+    id: 1,
+    title: 'Q&A2',
+    name: '관리자',
+    date: '0000. 00. 00.',
+    question: '이거는 어떻게 하나요?2',
+    answer: '이렇게 합니다2',
+    url: '/QApage/QApage/1',
+  },
+  {
+    id: 2,
+    title: 'Q&A3',
+    name: '관리자',
+    date: '0000. 00. 00.',
+    question: '이거는 어떻게 하나요?3',
+    answer: '이렇게 합니다3',
+    url: '/QApage/QApage/2',
+  },
+  {
+    id: 3,
+    title: 'Q&A4',
+    name: '관리자',
+    date: '0000. 00. 00.',
+    question: '이거는 어떻게 하나요?4',
+    answer: '이렇게 합니다4',
+    url: '/QApage/QApage/3',
+  },
+]
+export default QAdata
