ceffc7e47d818935c36d676cf59dad0ca7bfc08b
[Github <jungu2008@naver.com>
2022-03-18 01:02:38 +0900

    브런치 정리

---
+++ pages/signjoin1.vue
@@ -0,0 +1,132 @@
+template>
+    <div>
+        <header class="pb-5">
+            <h1 class="pb-3">govey</h1>
+            <hr>
+        </header>
+        <nav class="pt-5 pb-3">
+            <v-container>
+                <v-row justify="space-around">
+                    <div>
+                        <h1>회원가입</h1><br>
+                        고베이에 오신걸 환영합니다.
+                        <div class="d-flex pt-5 pl-5">
+                        <div class="pr-2"><v-img src="/kakao.png" height="50px" width="50px"></v-img></div>
+                        <div class="pr-2"><v-img src="/naver.png" height="50px" width="50px"></v-img></div>
+                        <div><v-img src="/google.svg" height="50px" width="50px"></v-img></div>
+                    </div>
+                    </div>
+                    <div>
+                        아직 고베이 계정이 있으세요?<br><br>
+                        <v-btn class="float-right" href="/login">
+                            로그인
+                        </v-btn>
+                    </div>
+                    
+                </v-row>
+                    
+            </v-container>
+        </nav>
+        <hr >
+        <section class="pt-3">
+            <v-container style=" width:50%; ">
+                    <div>
+                        
+                        <v-text-field label="example@gamil.com"></v-text-field>
+                    </div>
+                    <div>
+                        
+                        <v-text-field type="password" label="비밀번호"></v-text-field>
+                    </div>
+                    <div>
+                        
+                        <v-text-field type="password" label="비밀번호 확인"></v-text-field>
+                    </div>
+                    <div>
+                        
+                        <v-text-field label="이름"></v-text-field>
+                    </div>
+                    <div>
+                        
+                        <v-text-field label="닉네임"></v-text-field>
+                    </div>
+                    <div>
+                        
+                        <v-select
+                        :items="['남자', '여자', '무응답',]"
+                        label="성별"
+                        >
+                        <template v-slot:item="{ item, attrs, on }">
+                        <v-list-item
+                            v-bind="attrs"
+                            v-on="on"
+                        >
+                            <v-list-item-title
+                            :id="attrs['aria-labelledby']"
+                            v-text="item"
+                            ></v-list-item-title>
+                        </v-list-item>
+                        </template>
+                    </v-select>
+                    </div>
+                    <div>
+                        <div class="mb-6">Active picker: <code>{{ activePicker || 'null' }}</code></div>
+                            <v-menu
+                            ref="menu"
+                            v-model="menu"
+                            :close-on-content-click="false"
+                            transition="scale-transition"
+                            offset-y
+                            min-width="auto"
+                            >
+                            <template v-slot:activator="{ on, attrs }">
+                                <v-text-field
+                                v-model="date"
+                                label="생년월일"
+                                prepend-icon="mdi-calendar"
+                                readonly
+                                v-bind="attrs"
+                                v-on="on"
+                                ></v-text-field>
+                            </template>
+                            <v-date-picker
+                                v-model="date"
+                                :active-picker.sync="activePicker"
+                                :max="(new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10)"
+                                min="1950-01-01"
+                                @change="save"
+                            ></v-date-picker>
+                            </v-menu>
+                    </div>
+                    <v-btn href="/signjoin2">
+                        다음
+                    </v-btn>
+            </v-container>
+        </section>
+        <footer>
+            <hr>
+            Copyright © goveycompany. All rights reserved.
+        </footer>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'signjoin1-Page',
+ data: () => ({
+      activePicker: null,
+      date: null,
+      menu: false,
+    }),
+    watch: {
+      menu (val) {
+        val && setTimeout(() => (this.activePicker = 'YEAR'))
+      },
+    },
+    methods: {
+      save (date) {
+        this.$refs.menu.save(date)
+      },
+    },
+}
+</script>
---
+++ pages/signjoin2.vue
@@ -0,0 +1,82 @@
+<template>
+<v-container fluid style="width:80%">
+    <div>
+        <header class="pb-5">
+            <h1 class="pb-3">govey</h1>
+            <hr>
+        </header>
+        <v-container>
+        <section class="pt-3">
+            <div class="text-center pb-4">
+                <span style="font-size:40px">고베이에 오신걸 환영합니다!</span>
+            </div>
+            <br><br>
+            <div class="text-center pt-4">
+                <v-row>
+                    <v-col>
+                        <span style="font-size:30px">관심사 등록</span>
+                    </v-col>
+                    <v-col>
+                        <v-btn>다음에 할게요</v-btn>
+                    </v-col>
+                </v-row>
+            </div>
+            <br><br>
+            <div class="text-center">
+                관심사를 기준으로 설문을 추천하고 알려드려요!
+            </div>
+            <br><br>
+            <div class="text-center">
+                <v-row>
+                    <v-col v-for="(inter,i) in 3" :key="inter">
+                        <v-card
+                        class=" blue"
+                        tile
+                        outlined
+                        height="100px"
+                        >
+                        <v-card-title>   
+                            {{sections[i]}}
+                        </v-card-title>
+                        </v-card>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col v-for="(que,i) in 3" :key="que">
+                        <v-card
+                        class=" blue"
+                        tile
+                        outlined
+                        height="100px"
+                        >
+                        <v-card-title>   
+                            {{sections2[i]}}
+                        </v-card-title>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </div>
+
+
+
+        </section>
+        </v-container>
+        <footer>
+            <hr>
+            Copyright © goveycompany. All rights reserved.
+        </footer>
+    </div>
+</v-container>
+</template>
+
+<script>
+export default {
+  name: 'signjoin2-Page',
+   data(){
+       return{
+           sections : ['코딩', '대학생활', '취업'],
+           sections2 : ['패션', '디지털', '부동산']
+       }
+   }
+}
+</script>
