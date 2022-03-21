7eadcc107af73f9c88f4617223e2d7cb8d23f928
junpanda <jungu2008@naver.com>
2022-03-21 23:54:35 +0900

    카드내용 추가

--- pages/surveymaker.vue
+++ pages/surveymaker.vue
@@ -21,12 +21,8 @@
                 </v-card-subtitle>
             </v-card>
            </div>
-           <div class="pt-5">
-               <v-card
-                elevation="2"
-                outlined
-                class="red"
-                >
+           <div class="pt-5" id="bigboxplus">
+               <div class="check">
                 <v-row>
                     <v-col cols="6">
                         <input placeholder="제목 없는 질문">
@@ -38,28 +34,32 @@
                         박스
                     </v-col>
                 </v-row>
-                <div id="boxplus">
-                    <div id="box1">
+                <div class="boxplus">
+                    <div class="que_list1">
                         <v-row>설문 내용 추가</v-row>
                     </div>
                 </div>
                 <v-row class="pt-3">
                     <div>
-                        <v-btn id="btn1">
+                        <button type="button" class="btn1">
                             생성
-                        </v-btn>
-                        <v-btn id="btn2">
+                        </button>
+                       <button type="button" class="btn2">
                             삭제
-                        </v-btn>
+                        </button>
                     </div>
                 </v-row>
-               </v-card>
+               </div>
            </div>
-
+           <div>
+               <v-btn id="card_btn1">
+                   카드 생성
+               </v-btn>
+           </div>
        </section> 
         <footer>
             <hr>
-            <div class="text-md-center">
+            <div class="text-center">
                 Copyright © goveycompany. All rights reserved.
             </div>
         </footer>
@@ -75,15 +75,91 @@ export default {
         
     mounted(){
         var i = 1;
-        $("#btn1").click(function(){
+        $(".btn1").click(function(){
             i++;
-            $("#boxplus").append(
-                "<div id='box"+i+"'><v-row>추가"+i+"</v-row></div>"
+            $(".boxplus").append(
+                `<div class="que_list${i}"><v-row>추가${i}</v-row></div>`
             );
         });
-        $("#btn2").click(function(){
-            $("box"+i+"").remove();
+        $(".btn2").click(function(){
+            $(".que_list"+i+"").remove();
             i--;
+        });
+        var k = 2;
+        let $box = `<div id="pluscheck${k}">
+                <v-row>
+                    <v-col cols="6">
+                        <input placeholder="제목 없는 질문">
+                    </v-col>
+                    <v-col cols="2">
+                        사진
+                    </v-col>
+                    <v-col cols="4">
+                        박스
+                    </v-col>
+                </v-row>
+                <div class="boxplus${k}">
+                    <div class="que_list1">
+                        <v-row>설문 내용 추가</v-row>
+                    </div>
+                </div>
+                <v-row class="pt-3">
+                    <div>
+                        <button type="button" class="btn1">
+                            생성
+                        </button>
+                       <button type="button" class="btn2">
+                            삭제
+                        </button>
+                    </div>
+                </v-row>
+               </div>`;
+        $("#card_btn1").click(function(){
+           $("#bigboxplus").append(
+               `<div id="pluscheck${k}">
+                <v-row>
+                    <v-col cols="6">
+                        <input placeholder="제목 없는 질문">
+                    </v-col>
+                    <v-col cols="2">
+                        사진
+                    </v-col>
+                    <v-col cols="4">
+                        박스
+                    </v-col>
+                </v-row>
+                <div class="boxplus${k}">
+                    <div class="que_list1">
+                        <v-row>설문 내용 추가</v-row>
+                    </div>
+                </div>
+                <v-row class="pt-3">
+                    <div>
+                        <button type="button" class="btn1">
+                            생성
+                        </button>
+                       <button type="button" class="btn2">
+                            삭제
+                        </button>
+                    </div>
+                </v-row>
+               </div>`
+           );
+           k++;
+           });
+
+        var a = 2;
+        $("#bigboxplus").on('click','.btn1',function(){
+            $(".boxplus2").append(
+                `<div class="que_list${a}"><v-row>추가${a}</v-row></div>`
+            );
+            a++;
+        });
+         $("#bigboxplus").on('click','.btn1',function(){
+            $(".boxplus3").append(
+                `<div class="que_list${a}"><v-row>추가${a}</v-row></div>`
+            );
+            a++;
         });
     }
   
