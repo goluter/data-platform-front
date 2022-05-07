edb3330dd79e174833e4cc373ec84cc37330f2de
[Github <jungu2008@naver.com>
2022-05-07 22:31:06 +0900

    공통 레이아웃 헤더 푸터 추가

---
+++ layouts/layout_footer.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        
+    <v-footer class="footer">
+        
+        <v-row>
+            <v-col cols="3" class="text-md-center">
+                <NuxtLink to="/" style="color:black; text-decoration-line: none;">
+                    <div><img src="../assets/home.png"/></div>
+                    <div>홈</div>
+                </NuxtLink>
+            </v-col>
+            <v-col cols="3" class="text-md-center">
+                <NuxtLink to="/list/" style="color:black; text-decoration-line: none;">
+                    <div><img src="../assets/survey.png"/></div>
+                    <div>설문</div>
+                </NuxtLink>
+            </v-col>
+            <v-col cols="3" class="text-md-center">
+                <NuxtLink to="/" style="color:black; text-decoration-line: none;">
+                    <div><img src="../assets/store.png"/></div>
+                    <div>스토어</div>
+                </NuxtLink>
+            </v-col>
+            <v-col cols="3" class="text-md-center">
+                <NuxtLink to="/meminfo/" style="color:black; text-decoration-line: none;">
+                    <div><img src="../assets/Male User.png"/></div>
+                    <div>마이</div>
+                </NuxtLink>
+            </v-col>
+        </v-row>
+    </v-footer>
+    </div>
+</template>
+
+<script>
+
+export default{
+    name: 'layout_footer',
+    layout: 'DefaultLayout'
+    
+}
+</script>
+<style>
+.footer{
+    position: fixed;
+    bottom: 50px;
+    width: 100%;
+}
+</style>
--- pages/m_index.vue
+++ pages/m_index.vue
@@ -1,17 +1,24 @@
 <template>
     <div>
         <Header />
+        <div>
+            하이로
+        </div>
+        <Footer />
     </div>
+    
 </template>
 
 <script>
     import Header from '/layouts/layout_header.vue'
+    import Footer from '/layouts/layout_footer.vue'
 
     export default{
         name: 'm_index',
         layout: 'DefaultLayout',
         components:{
             Header,
+            Footer
         }
     }
 </script>
