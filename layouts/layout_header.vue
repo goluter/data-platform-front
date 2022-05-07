23a0bfcbb0cc2d9f577d25c2df5301427ba86c2d
[Github <jungu2008@naver.com>
2022-05-07 10:36:02 +0900

    레이아웃 헤더 생성

---
+++ layouts/layout_header.vue
@@ -0,0 +1,101 @@
+<template>
+    <v-toolbar dense>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-navigation-drawer 
+      app v-model="drawer" 
+      absolute
+      temporary >
+        <v-list
+          nav
+          dense
+        >
+          <v-list-item-group
+            v-model="group"
+            active-class="deep-purple--text text--accent-4"
+          >
+            <v-list-item>
+              <v-list-item-title>
+                <NuxtLink to="/" style="color:black; text-decoration-line: none;">리포트</NuxtLink>
+              </v-list-item-title>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-title>
+                <NuxtLink to="/" style="color:black; text-decoration-line: none;">공지사항</NuxtLink>
+              </v-list-item-title>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-title>
+                <NuxtLink to="/" style="color:black; text-decoration-line: none;">이벤트</NuxtLink>
+              </v-list-item-title>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-title>
+                <NuxtLink to="/" style="color:black; text-decoration-line: none;">명예의전당</NuxtLink>
+              </v-list-item-title>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-title>
+                <NuxtLink to="/" style="color:black; text-decoration-line: none;">고객센터</NuxtLink>
+              </v-list-item-title>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-title>
+                <NuxtLink to="/" style="color:black; text-decoration-line: none;">개인정보처리방침</NuxtLink>
+              </v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title>
+                <NuxtLink to="/" style="color:black; text-decoration-line: none;">이용약관</NuxtLink>
+              </v-list-item-title>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+        <div class="sns_link" style="margin-top :110px;">
+          <a href="https://www.youtube.com/channel/UCpj4Mj4OFgl5T0kTk1T3L_w"><img src="../assets/image 4.png"/></a>
+          <a href="https://www.instagram.com/govey_official/?hl=ko"><img src="../assets/image 5.png"/></a>
+          <a href="https://www.facebook.com/고베이-104289228923939"><img src="../assets/image 6.png"/></a>
+        </div>
+      </v-navigation-drawer>
+      
+
+      <v-spacer></v-spacer>
+      <v-toolbar-title>govey</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <img src="../assets/Vector.png"/>
+      </v-btn>
+
+      <v-btn icon>
+        <img src="../assets/Notification.png"/>
+      </v-btn>
+
+    </v-toolbar>
+</template>
+
+<script>
+
+export default{
+    name: 'layout_header',
+    layout: 'DefaultLayout',
+    data(){
+        return{
+            drawer:false,
+            group: null,
+        }
+    },
+    watch: {
+      group () {
+        this.drawer = false
+      },
+    },
+}
+</script>
+<style>
+
+</style>
---
+++ pages/m_index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <Header />
+    </div>
+</template>
+
+<script>
+    import Header from '/layouts/layout_header.vue'
+
+    export default{
+        name: 'm_index',
+        layout: 'DefaultLayout',
+        components:{
+            Header,
+        }
+    }
+</script>
+
