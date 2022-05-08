6155732236a9667a38135747874205ce60250139
nbmyng <nbmyng@gmail.com>
2022-05-09 02:00:00 +0900

    55-프론트-nuxt-연동

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,11 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    proxy: true
+  },
+
+  proxy: {
+    '/api': 'http://localhost:8080'
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
