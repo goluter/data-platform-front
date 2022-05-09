4e0403b4a73614c4f28df058f98ed66e1a838e32
nbmyng <nbmyng@gmail.com>
2022-05-09 14:03:52 +0900

    소셜 로그인 버튼 스타일 적용

--- pages/login.vue
+++ pages/login.vue
@@ -1,48 +1,48 @@
 <template>
-<div id="ct">
-  <div class="header">
-    <a href="/">govey</a>
-  </div>
-  <div class="wrapper">
-    <div class="welcome">
-      <h1>로그인</h1>
-      <span>고베이에 오신 것을 환영합니다!</span>
-      <div class="signup">
-        <p>아직 고베이 계정이 없으신가요?</p>
-        <a href="/signup">회원가입</a>
+  <div id="ct">
+    <div class="header">
+      <a href="/">govey</a>
+    </div>
+    <div class="wrapper">
+      <div class="welcome">
+        <h1>로그인</h1>
+        <span>고베이에 오신 것을 환영합니다!</span>
+        <div class="signup">
+          <p>아직 고베이 계정이 없으신가요?</p>
+          <a href="/signup">회원가입</a>
+        </div>
+      </div>
+      <div class="login-type">
+        <div class="social-login">
+          <button class="kakao"><v-img src="/kakao.png" height="50px" width="50px"></v-img></button>
+          <button class="naver"><v-img src="/naver.png" height="50px" width="50px"></v-img></button>
+          <button class="google"><v-img src="/google.svg" height="50px" width="50px"></v-img></button>
+        </div>
+        <p>또는 이메일 로그인</p>
+      </div>
+      <div class="login">
+        <form action="" method="post">
+          <div class="input">
+            <label>이메일</label>
+            <input type="email" id="account" placeholder="이메일을 입력하세요." required>
+          </div>
+          <div class="input">
+            <label>비밀번호</label>
+            <input type="password" id="password" placeholder="비밀번호를 입력하세요." required>
+          </div>
+          <div id="login-option">
+            <div class="auto-login">
+              <input type="checkbox"><label>자동로그인</label>
+            </div>
+            <div id="forgot">
+              <a href="/">비밀번호 찾기</a>
+            </div>
+          </div>
+          <input type="submit" value="로그인">
+        </form>
       </div>
     </div>
-    <div class="login-type">
-      <div class="social-login">
-        <button class="kakao"><v-img src="/kakao.png" height="50px" width="50px"></v-img></button>
-        <button class="naver"><v-img src="/naver.png" height="50px" width="50px"></v-img></button>
-        <button class="google"><v-img src="/google.svg" height="50px" width="50px"></v-img></button>
-      </div>
-      <p>또는 이메일 로그인</p>
-    </div>
-    <div class="login">
-      <form action="" method="post">
-        <div class="input">
-          <label>이메일</label>
-          <input type="email" id="account" placeholder="이메일을 입력하세요." required>
-        </div>
-        <div class="input">
-          <label>비밀번호</label>
-          <input type="password" id="password" placeholder="비밀번호를 입력하세요." required>
-        </div>
-        <div id="login-option">
-          <div class="auto-login">
-            <input type="checkbox"><label>자동로그인</label>
-          </div>
-          <div id="forgot">
-            <a href="/">비밀번호 찾기</a>
-          </div>
-        </div>
-        <input type="submit" value="로그인">
-      </form>
-    </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -77,11 +77,11 @@ export default {
 }
 
 .welcome {
-  padding: 80px 0 0 0;
+  padding: 80px 0 30px 0;
 }
 
 .signup {
-  margin-top: 30px;
+  margin-top: 40px;
   text-align: right;
 }
 
@@ -99,13 +99,19 @@ export default {
 }
 
 .login-type .social-login {
-  margin-bottom: 30px;
+  margin-bottom: 30px; padding-bottom: 30px;
   border-bottom: 1px solid #ddd;
 }
 
 .login-type button {
   display: inline-block;
-  padding: 30px 5px;
+  width: 50px; height: 50px;
+  margin: 0 5px; border-radius: 70%; overflow: hidden;
+}
+
+.login-type v-img {
+  width: 100%; height: 100%;
+  object-fit: cover;
 }
 
 .login {
@@ -132,5 +138,4 @@ export default {
 #login-option {
   display: flex; justify-content: space-between;
 }
-
 </style>
