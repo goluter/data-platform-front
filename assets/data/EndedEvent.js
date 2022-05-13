ac8749dc1cc3c54f1c8abfd00997fb630c4c6fec
LEE <vine127@gmail.com>
2022-05-13 22:34:02 +0900

    이벤트 목록 & 이벤트 상세 페이지

---
+++ assets/data/EndedEvent.js
@@ -0,0 +1,17 @@
+var EndedEvent = [
+  {
+    id: 0,
+    title: '중간고사 이벤트',
+    date: '0000. 00. 00. ~ 0000.00.01',
+    url: '/EndedEvent/0',
+    maininfo: '중간고사',
+  },
+  {
+    id: 1,
+    title: '포켓몬빵 1+1 이벤트!',
+    date: '2022. 05. 10. ~ 2022.05.11',
+    url: '/EndedEvent/1',
+    maininfo: '피카츄 망고컵케익',
+  },
+]
+export default EndedEvent
