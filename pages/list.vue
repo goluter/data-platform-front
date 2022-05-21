6826498ac93cf698ca23ef1f68566521d9592034
[Github <jungu2008@naver.com>
2022-05-21 13:17:34 +0900

    수정

--- pages/list.vue
+++ pages/list.vue
@@ -25,30 +25,26 @@
     <div class="gray-line">
     </div>
     <div class="ma-2">
-      <v-row justify="start">
-        <v-col cols="2">
-          <div class="gray-sbox">
-            #태그
-          </div>
-        </v-col>
-        <v-col cols="2">
-          <div class="gray-sbox">
-            #태그
-          </div>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="3">
-          검색
-        </v-col>
-        <v-col cols="4">
+      <v-row class="mt-5">
+        <v-col cols="5">
           <v-autocomplete
-            v-model="a"
+            v-model="tag_model"
             filled
             rounded
             solo
             dense
-            :items="b"
+            :items="tag_models"
+            label="태그"
+          ></v-autocomplete>
+        </v-col>
+        <v-col cols="5">
+          <v-autocomplete
+            v-model="gift_model"
+            filled
+            rounded
+            solo
+            dense
+            :items="gift_models"
             label="보상종류"
           ></v-autocomplete>
         </v-col>
@@ -58,9 +54,9 @@
     </div>
     <div v-if="tab1 == true">
       <div class="mt-3 d-flex justify-end">
-        <div style="width:30%;">
+        <div style="width:35%;">
         <v-autocomplete
-        v-model="automodel"
+        v-model="auto_model"
         filled
         rounded
         dense
@@ -68,28 +64,31 @@
         ></v-autocomplete>
         </div>
       </div>
-      <div class="mt-3" style="position:relative;">
-        <v-row>
-          <v-col cols="4">
-            <div class="rounded-xl gray-box"></div>
-          </v-col>
-          <v-col cols="6" >
-            <v-row class="mt-1">
-              상명대학교 학생들에게 물어봅니다.
-            </v-row>
-            <v-row>
-              2022.01.12~2022.04.23
-            </v-row>
-            <v-row style="font-size:9px; position:absolute; bottom:25px;" >
-              <div class="mr-7">
-                114,300명이 참여 중입니다.
-              </div>
-              <div>
-                #대학생 #새학기 #축제
-              </div>
-            </v-row>
-          </v-col>
-        </v-row>
+      <div v-for="(list,i) in 5" :key="list">
+        <div class="mt-3" style="position:relative;">
+          <v-row>
+            <v-col cols="4">
+              <div class="rounded-xl gray-box"></div>
+            </v-col>
+            <v-col cols="6" >
+              <v-row class="mt-1">
+                상명대학교 학생들에게 물어봅니다.
+              </v-row>
+              <v-row>
+                2022.01.12~2022.04.23
+              </v-row>
+              <v-row style="font-size:9px; position:absolute; bottom:25px;" >
+                <div class="mr-7">
+                  114,300명이 참여 중입니다.
+                </div>
+                <div>
+                  #대학생 #새학기 #축제
+                </div>
+              </v-row>
+            </v-col>
+          </v-row>
+        </div>
+        <div class="blue_box mt-2"></div>
       </div>
     </div>
   </v-container>
@@ -107,10 +106,12 @@ export default {
       tab1 : true,
       tab2 : false,
       tab3 : false,
-      automodel : "최신순",
+      auto_model : "최신순",
       auto_models:["최신순","참여순","포인트순"],
-      a : null,
-      b : ["포인트","기프티콘"]
+      gift_model : null,
+      gift_models : ["포인트","기프티콘"],
+      tag_model : null,
+      tag_models : ["대학생","사회생활","코딩"]
     }
   },
   
@@ -122,7 +123,7 @@ export default {
 
 <style>
 .wrap{
-    max-width: 500px;
+    max-width: 450px;
 }
 .gray-line{
   width:100%;   
@@ -140,5 +141,10 @@ export default {
 }
 .gray-sbox{
   background-color:lightgray;
+}
+.blue_box{
+  background-color: #83e0fd;
+  height: 41px;
+  widows: 100%;
 }
 </style>
