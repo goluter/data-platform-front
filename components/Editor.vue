15ed900fad0eb0bcff7965e1c4e74ef368adea5f
nbmyng <nbmyng@gmail.com>
2022-06-01 19:46:11 +0900

    add 페이지 / tiptap editor 컴포넌트

---
+++ components/Editor.vue
@@ -0,0 +1,107 @@
+<template>
+  <div v-if="editor">
+    <button @click="editor.chain().focus().setTextAlign('left').run()" :class="{ 'is-active': editor.isActive({ textAlign: 'left' }) }">
+      <v-icon>mdi-format-align-left</v-icon>
+    </button>
+    <button @click="editor.chain().focus().setTextAlign('center').run()" :class="{ 'is-active': editor.isActive({ textAlign: 'center' }) }">
+      <v-icon>mdi-format-align-center</v-icon>
+    </button>
+    <button @click="editor.chain().focus().setTextAlign('right').run()" :class="{ 'is-active': editor.isActive({ textAlign: 'right' }) }">
+      <v-icon>mdi-format-align-right</v-icon>
+    </button>
+    <button @click="editor.chain().focus().setTextAlign('justify').run()" :class="{ 'is-active': editor.isActive({ textAlign: 'justify' }) }">
+      <v-icon>mdi-format-align-justify</v-icon>
+    </button>
+    <v-row>
+      <v-col cols="12"></v-col>
+    </v-row>
+    <floating-menu :editor="editor" :tippy-options="{ duration: 100 }" v-if="editor">
+      <button @click="editor.chain().focus().toggleHeading({ level: 1 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }">
+        제목1
+      </button>
+      <button @click="editor.chain().focus().toggleHeading({ level: 2 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }">
+        제목2
+      </button>
+      <button @click="editor.chain().focus().toggleBulletList().run()" :class="{ 'is-active': editor.isActive('bulletList') }">
+        글머리표
+      </button>
+      <button @click="editor.chain().focus().toggleOrderedList().run()" :class="{ 'is-active': editor.isActive('orderedList') }">
+        문단번호
+      </button>
+    </floating-menu>
+    <bubble-menu :editor="editor" :tippy-options="{ duration: 100 }">
+      <button @click="editor.chain().focus().toggleBold().run()" :class="{ 'is-active': editor.isActive('bold') }">
+        굵게
+      </button>
+      <button @click="editor.chain().focus().toggleStrike().run()" :class="{ 'is-active': editor.isActive('strike') }">
+        취소선
+      </button>
+    </bubble-menu>
+    <editor-content :editor="editor" />
+    <v-row style="background-color: #f7f7f8">
+      <v-col cols="12"></v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <v-btn class="camera" icon @click="addImage">
+          <v-icon>mdi-camera-plus</v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import { Editor, EditorContent, BubbleMenu, FloatingMenu } from '@tiptap/vue-2'
+import StarterKit from '@tiptap/starter-kit'
+import TextAlign from '@tiptap/extension-text-align'
+import Image from '@tiptap/extension-image'
+import Dropcursor from '@tiptap/extension-dropcursor'
+
+export default {
+  components: {
+    EditorContent,
+    BubbleMenu,
+    FloatingMenu
+  },
+
+  data() {
+    return {
+      editor: null,
+    }
+  },
+  mounted() {
+    this.editor = new Editor({
+      content: '<p></p><p></p><p></p>',
+      extensions: [
+        StarterKit,
+        TextAlign,
+        Image,
+        Dropcursor,
+      ],
+    })
+  },
+  methods: {
+    addImage() {
+      const url = window.prompt('URL')
+
+      if (url) {
+        this.editor.chain().focus().setImage({ src: url }).run()
+      }
+    },
+  },
+  beforeDestroy() {
+    this.editor.destroy()
+  },
+}
+</script>
+<style scoped>
+button {
+  border: solid 1px;
+  border-radius: 3px;
+  font-size: 14px;
+}
+.camera {
+  border: none;
+}
+</style>
--- pages/reports/add.vue
+++ pages/reports/add.vue
@@ -1,13 +1,111 @@
 <template>
-
+  <div>
+    <v-container>
+      <v-row>
+        <v-col cols="8" md="10"></v-col>
+        <v-col cols="4" md="2">
+          <v-btn icon><v-icon>mdi-content-save-outline</v-icon></v-btn>
+          <v-btn color="teal accent-4" style="color: white; font-weight: 500">등록</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-row class="mt-0 mb-3">
+      <v-col class="pa-0" cols="12">
+        <v-divider></v-divider>
+      </v-col>
+    </v-row>
+    <v-container>
+      <v-row>
+        <v-col cols="12">
+          제목
+          <v-text-field
+              label="제목을 입력하세요."
+              filled
+              flat
+              single-line
+              color="teal accent-4"
+          >
+          </v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          태그
+          <v-combobox
+              v-model="init"
+              :search-input.sync="search"
+              :items="tags"
+              label="태그를 입력하세요."
+              multiple
+              chips
+              deletable-chips
+              filled
+              flat
+              single-line
+              color="teal accent-4"
+          >
+            <template v-slot:no-data>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>
+                    <span style="font-size: 12px"><strong>{{ search }}</strong> 태그가 없습니다. <kbd>Enter</kbd>를 눌러 생성할 수 있습니다.</span>
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </template>
+          </v-combobox>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" style="margin-bottom: 30px">
+          분석 설문
+          <survey-card :survey-data="surveyData" />
+        </v-col>
+      </v-row>
+      <v-row style="background-color: #f7f7f8">
+        <v-col cols="12"></v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <Editor></Editor>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
+import SurveyCard from "../../components/SurveyCard.vue";
+import Editor from "../../components/Editor.vue";
+
 export default {
-  name: "add"
+  components: {Editor, SurveyCard},
+  layout: 'empty',
+  data() {
+    return {
+      search: null,
+      surveyData: [
+        {
+          title: '대학생들에게 묻습니다',
+          left: '4',
+          count: '114,300',
+          tags: ['대학생', '새내기', '축제'],
+          rewards: [
+            { title: '스타벅스', icon: 'mdi-gift', color: 'red' },
+            {
+              title: '100P',
+              icon: 'mdi-circle-multiple',
+              color: 'yellow darken-3'
+            }
+          ]
+        }
+      ],
+      init: [],
+      tags: ['대학생', '직장인']
+    }
+  }
 }
 </script>
 
 <style scoped>
-
 </style>
