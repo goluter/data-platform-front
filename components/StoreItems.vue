9697a9aa9e19fcc945f9428b807c2bfe4d95df78
nbmyng <nbmyng@gmail.com>
2022-06-02 17:24:08 +0900

    store 페이지 / StoreItems 컴포넌트

---
+++ components/StoreItems.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div
+        v-for="(item, i) in storeData"
+        :key="i"
+    >
+      <v-row class="pa-3">
+        <v-col class="d-flex justify-center" cols="3" md="2">
+          <div class="image">
+          </div>
+        </v-col>
+        <v-col class="pa-auto" cols="9" md="10" style="display: flex; flex-direction: column; justify-content: space-evenly">
+          <span class="item-title">
+            {{ item.title }}
+          </span>
+          <span class="item-price">
+            {{ item.price }} P
+          </span>
+        </v-col>
+      </v-row>
+      <v-divider></v-divider>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    storeData: {
+      type: Array
+    }
+  }
+}
+</script>
+
+<style scoped>
+.image {
+  padding: 3%;
+  width: 60px;
+  height: 60px;
+  border: #f0f0f0 solid 1px;
+  border-radius: 50%;
+}
+.item-title {
+  display: block;
+  font-size: 12px;
+  font-weight: 600;
+}
+.item-price {
+  font-size: 12px;
+}
+</style>
---
+++ pages/store/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <v-container class="pa-0">
+      <v-row
+          class="pt-3"
+      >
+        <v-col cols="1">
+        </v-col>
+        <v-col class="ml-auto" cols="auto">
+          구매내역
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col
+            class="pa-4 pl-7"
+            cols="12"
+            style="background-color: #d7f5ff"
+        >
+          <span class="guide">적립 방법 알아보기 <v-icon>mdi-chevron-right</v-icon></span>
+        </v-col>
+      </v-row>
+      <StoreItems :store-data="storeData"></StoreItems>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import StoreItems from "../../components/StoreItems.vue";
+export default {
+  components: {StoreItems},
+  layout: 'empty',
+  data() {
+    return {
+      storeData: [
+        {title: '[스타벅스] 카페 아메리카노 T', price: 4000 },
+        {title: '[BHC] 뿌링클콤보', price: 12000 },
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.guide {
+  font-size: 16px;
+  font-weight: 600;
+}
+</style>
