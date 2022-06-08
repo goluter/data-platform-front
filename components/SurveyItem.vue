a20b70eae6a436b444b5977293acce8d7eeb8ec4
nbmyng <nbmyng@gmail.com>
2022-06-08 15:44:53 +0900

    reports/item 페이지 / SurveyItem 컴포넌트

---
+++ components/SurveyItem.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="wrapper">
+    <div class="survey-wrapper">
+      <v-row>
+        <v-col class="survey-img-box col-3">
+          <div class="survey-img" />
+        </v-col>
+        <v-col class="survey-content col-9">
+          <span class="survey-title">
+            {{ surveyData.title }}
+          </span>
+          <span class="survey-period"> {{ surveyData.start }} ~ {{ surveyData.end }} </span>
+          <div class="survey-count-tags">
+            <span class="survey-count"> {{ surveyData.count | comma }}명이 참여 중 </span>
+            <span
+              v-for="(item, i) in surveyData.tags"
+              :key="i"
+              class="survey-tags"
+            >
+              <NuxtLink to="">#{{ item }}</NuxtLink>
+            </span>
+          </div>
+        </v-col>
+      </v-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    surveyData: {
+      type: Object
+    }
+  }
+}
+</script>
+
+<style scoped>
+.survey-img {
+  width: 100%;
+  height: 100%;
+  background-color: #a7a7a7;
+  border-radius: 14px;
+}
+.survey-content {
+  display: flex;
+  flex-direction: column;
+}
+.survey-title {
+  display: block;
+  font-weight: bold;
+  font-size: 14px;
+}
+.survey-period {
+  margin-bottom: 13px;
+  font-size: 10px;
+}
+.survey-count-tags {
+  font-size: 10px;
+}
+.survey-tags {
+  float: right;
+  margin: 1px;
+}
+.survey-tags a {
+  text-decoration: none;
+}
+</style>
