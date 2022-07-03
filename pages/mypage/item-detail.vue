a132a8d38f5928ee144ee01e16d3fb592279186a
nbmyng <nbmyng@gmail.com>
2022-07-03 17:12:07 +0900

    feat: alert after activated

--- pages/mypage/item-detail.vue
+++ pages/mypage/item-detail.vue
@@ -23,55 +23,75 @@
     </v-container>
     <v-container>
       <v-row>
-        <v-col class="pa-0" cols="12">
-          <div class="item-activate d-flex">
-            <v-dialog>
-              <template
-                #activator="{ on, attrs }"
+        <v-col cols="12">
+          <v-row>
+            <v-col class="pa-0 d-flex justify-content-center" cols="12">
+              <v-alert
+                v-model="activated"
+                dismissible
+                prominent
+                dense
+                rounded
+                type="success"
+                style="position: fixed; z-index: 1; margin: 0 auto; right: 0; bottom: 45px; left: 0; width: 90%; max-width: 900px;"
               >
-                <v-btn
-                  text
-                  color="transparent"
-                  v-bind="attrs"
-                  v-on="on"
-                >
-                  <span class="item-activate-btn">아이템 사용</span>
-                </v-btn>
-              </template>
-              <v-card class="pa-3">
-                <v-row>
-                  <v-col class="mx-auto mt-3" cols="auto">
-                    <span class="item-activate-card-title">
-                      아이템 사용 확정
-                    </span>
-                  </v-col>
-                </v-row>
-                <v-row>
-                  <v-col class="mx-auto" cols="12" md="auto">
-                    <span class="item-activate-card-text">
-                      아이템 사용 확정 링크는 인증된 이메일을 통해 전달됩니다.<br>
-                      탈취 및 분실에 대해 책임지지 않습니다. 보안에 유의 부탁드립니다.
-                    </span>
-                  </v-col>
-                </v-row>
-                <v-row>
-                  <v-col cols="auto" class="item-activate-card-checkbox mx-auto my-3">
-                    <form>
-                      <div class="d-flex align-items-center">
-                        <input v-model="confirmItemActivate" type="checkbox">
-                        <label class="ml-2" for="confirmItemActivate"><a class="term-link" href="#">아이템 사용 약관</a>에 동의하고 사용을 확정하겠습니다.</label><br>
-                      </div>
-                      <div class="d-flex mt-3" style="justify-content: center">
-                        <v-btn depressed color="#1a9efe" :disabled="!confirmItemActivate">
-                          <span class="item-activate-card-checkbox-btn">메일 발송</span>
-                        </v-btn>
-                      </div>
-                    </form>
-                  </v-col>
-                </v-row>
-              </v-card>
-            </v-dialog>
-          </div>
+                사용이 확정되었습니다.<br>이메일을 확인해주세요.
+              </v-alert>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col class="pa-0" cols="12">
+              <div class="item-activate d-flex">
+                <v-dialog v-model="dialog">
+                  <template
+                    #activator="{ on, attrs }"
+                  >
+                    <v-btn
+                      color="#1a9efe"
+                      v-bind="attrs"
+                      :disabled="activated"
+                      style="height: 55px; width: 100%;"
+                      v-on="on"
+                    >
+                      <span class="item-activate-btn-text">아이템 사용</span>
+                    </v-btn>
+                  </template>
+                  <v-card class="pa-3">
+                    <v-row>
+                      <v-col class="mx-auto mt-3" cols="auto">
+                        <span class="item-activate-card-title">
+                          아이템 사용 확정
+                        </span>
+                      </v-col>
+                    </v-row>
+                    <v-row>
+                      <v-col class="mx-auto" cols="12" md="auto">
+                        <span class="item-activate-card-text">
+                          아이템 사용 확정 링크는 인증된 이메일을 통해 전달됩니다.<br>
+                          탈취 및 분실에 대해 책임지지 않습니다. 보안에 유의 부탁드립니다.
+                        </span>
+                      </v-col>
+                    </v-row>
+                    <v-row>
+                      <v-col cols="auto" class="item-activate-card-checkbox mx-auto my-3">
+                        <form>
+                          <div class="d-flex align-items-center">
+                            <input v-model="confirmItemActivate" type="checkbox">
+                            <label class="ml-2" for="confirmItemActivate"><a class="term-link" href="#">아이템 사용 약관</a>에 동의하고 사용을 확정하겠습니다.</label><br>
+                          </div>
+                          <div class="d-flex mt-3" style="justify-content: center">
+                            <v-btn depressed color="#1a9efe" :disabled="!confirmItemActivate" @click="dialog=false; activated=true">
+                              <span class="item-activate-card-checkbox-btn">메일 발송</span>
+                            </v-btn>
+                          </div>
+                        </form>
+                      </v-col>
+                    </v-row>
+                  </v-card>
+                </v-dialog>
+              </div>
+            </v-col>
+          </v-row>
         </v-col>
       </v-row>
     </v-container>
@@ -82,7 +102,9 @@
 export default {
   data () {
     return {
+      dialog: false,
       confirmItemActivate: false,
+      activated: false,
       itemData: { name: '[스타벅스] 카페 아메리카노 T', provider: '스타벅스' }
     }
   },
@@ -94,7 +116,7 @@ export default {
 
 <style scoped>
 .wrapper {
-  height: calc(100% - 48px);
+  height: calc(100% - 49px);
 }
 .upper-body {
   height: calc(100% - 55px);
@@ -120,9 +142,8 @@ export default {
   justify-content: center;
   align-items: center;
   height: 55px;
-  background-color: #1a9efe;
 }
-.item-activate-btn {
+.item-activate-btn-text {
   font-size: 16px;
   font-weight: 600;
   color: white;
