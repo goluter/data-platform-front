2b47c6552341ab6998f7e5cddadc015bbe735351
nbmyng <nbmyng@gmail.com>
2022-07-03 15:46:31 +0900

    feat: confirm item activate

--- pages/mypage/item-detail.vue
+++ pages/mypage/item-detail.vue
@@ -25,7 +25,52 @@
       <v-row>
         <v-col class="pa-0" cols="12">
           <div class="item-activate d-flex">
-            <span>아이템 사용</span>
+            <v-dialog>
+              <template
+                #activator="{ on, attrs }"
+              >
+                <v-btn
+                  text
+                  color="transparent"
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  <span class="item-activate-btn">아이템 사용</span>
+                </v-btn>
+              </template>
+              <v-card class="pa-3">
+                <v-row>
+                  <v-col class="mx-auto mt-3" cols="auto">
+                    <span class="item-activate-card-title">
+                      아이템 사용 확정
+                    </span>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col class="mx-auto" cols="12" md="auto">
+                    <span class="item-activate-card-text">
+                      아이템 사용 확정 링크는 인증된 이메일을 통해 전달됩니다.<br>
+                      탈취 및 분실에 대해 책임지지 않습니다. 보안에 유의 부탁드립니다.
+                    </span>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="auto" class="item-activate-card-checkbox mx-auto my-3">
+                    <form>
+                      <div class="d-flex align-items-center">
+                        <input v-model="confirmItemActivate" type="checkbox">
+                        <label class="ml-2" for="confirmItemActivate"><a class="term-link" href="#">아이템 사용 약관</a>에 동의하고 사용을 확정하겠습니다.</label><br>
+                      </div>
+                      <div class="d-flex mt-3" style="justify-content: center">
+                        <v-btn depressed color="#1a9efe" :disabled="!confirmItemActivate">
+                          <span class="item-activate-card-checkbox-btn">메일 발송</span>
+                        </v-btn>
+                      </div>
+                    </form>
+                  </v-col>
+                </v-row>
+              </v-card>
+            </v-dialog>
           </div>
         </v-col>
       </v-row>
@@ -37,6 +82,7 @@
 export default {
   data () {
     return {
+      confirmItemActivate: false,
       itemData: { name: '[스타벅스] 카페 아메리카노 T', provider: '스타벅스' }
     }
   },
@@ -75,8 +121,27 @@ export default {
   align-items: center;
   height: 55px;
   background-color: #1a9efe;
+}
+.item-activate-btn {
   font-size: 16px;
   font-weight: 600;
+  color: white;
+}
+.item-activate-card-title {
+  font-size: 16px;
+  font-weight: 600;
+}
+.item-activate-card-text {
+  font-size: 12px;
+}
+.item-activate-card-checkbox {
+  font-size: 12px;
+}
+.term-link {
+  color: #1976d2 !important;
+}
+.item-activate-card-checkbox-btn {
+  font-size: 14px;
   color: white;
 }
 </style>
