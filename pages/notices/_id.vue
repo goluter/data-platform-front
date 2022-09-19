781dffac2a42b2c8648db24eefece7c9b934d595
nbmyng <nbmyng@gmail.com>
2022-09-19 21:10:24 +0900

    feat: grid notices, remove max-width css

--- pages/notices/_id.vue
+++ pages/notices/_id.vue
@@ -1,19 +1,23 @@
 <template>
-  <div>
-    <div style="border-bottom: 1px solid #d3d3d3">
-      <div class="noticetitle">
-        {{ NoticeData[$route.params.id].title }}
-      </div>
-      <div class="date">
-        {{ NoticeData[$route.params.id].date }}
-      </div>
-    </div>
-    <div>
-      <div class="noticemain">
-        {{ NoticeData[$route.params.id].answer }}
-      </div>
-    </div>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <div style="border-bottom: 1px solid #d3d3d3">
+          <div class="noticetitle">
+            {{ NoticeData[$route.params.id].title }}
+          </div>
+          <div class="date">
+            {{ NoticeData[$route.params.id].date }}
+          </div>
+        </div>
+        <div>
+          <div class="noticemain">
+            {{ NoticeData[$route.params.id].answer }}
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
