ef0e9a2c65bd9fc4c675058554085a7585b8bc01
nbmyng <nbmyng@gmail.com>
2022-09-19 21:07:59 +0900

    feat: remove header in notices/_id.vue

--- pages/notices/_id.vue
+++ pages/notices/_id.vue
@@ -1,16 +1,5 @@
 <template>
   <div>
-    <div class="header">
-      <NuxtLink to="/notice" style="color: black; text-decoration-line: none">
-        <img
-          style="position: absolute"
-          src="../../assets/Arrow back ios.png"
-          ALIGN="left"
-        >
-      </NuxtLink>
-
-      <a><span>공지사항</span></a>
-    </div>
     <div style="border-bottom: 1px solid #d3d3d3">
       <div class="noticetitle">
         {{ NoticeData[$route.params.id].title }}
