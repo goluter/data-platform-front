5f8e107529d976ef3e8820c896780ee4014f1d3c
nbmyng <nbmyng@gmail.com>
2022-10-01 21:57:41 +0900

    feat: survey view

--- pages/surveys/view.vue
+++ pages/surveys/view.vue
@@ -73,25 +73,25 @@
         </v-col>
       </v-row>
       <v-row>
-        <v-col class="pa-4" cols="12">
-          <!--          {{ surveyData.content }}-->
-          {{ surveyData.content | trunc(100, '...') }}
+        <v-col v-show="!survVisible" cols="12">
+          {{ surveyContent | trunc(200, '...') }}
+        </v-col>
+        <v-expand-transition>
+          <v-col v-show="survVisible" cols="12">
+            {{ surveyContent }}
+          </v-col>
+        </v-expand-transition>
+      </v-row>
+      <v-row>
+        <v-col class="pa-2 pt-0" cols="12">
+          <button class="survey-more" @click="survVisible=!survVisible">
+            <span class="survey-more-text">자세히 보기</span>
+            <v-icon class="ml-auto">
+              mdi-chevron-right
+            </v-icon>
+          </button>
         </v-col>
       </v-row>
-      <!--      <v-row v-if="showToggle">-->
-      <!--        <v-col class="pa-2 pb-1" cols="12">-->
-      <!--          <button style="font-size: 11px; font-weight: 600;">-->
-      <!--            <div class="survey-more">-->
-      <!--              <div style="display: flex; align-items: center">-->
-      <!--                자세히 보기-->
-      <!--              </div>-->
-      <!--              <v-icon class="ml-auto">-->
-      <!--                mdi-chevron-right-->
-      <!--              </v-icon>-->
-      <!--            </div>-->
-      <!--          </button>-->
-      <!--        </v-col>-->
-      <!--      </v-row>-->
       <v-row>
         <v-col class="pa-4" cols="12" style="background-color: #eeeeee" />
       </v-row>
@@ -115,35 +115,75 @@
                 <v-row style="padding: 5px;">
                   <v-col class="pa-2 pb-5" cols="12">
                     <div v-for="(item, i) in surveyStats" :key="i" class="survey-stat-box">
-                      <span class="survey-stat-box-title">항목</span>
-                      <span class="survey-stat-box-desc">{{ item.questions }}개</span>
-                      <span class="survey-stat-box-more">자세히 보기</span>
+                      <span class="survey-stat-box-title">{{ item.type }}</span>
+                      <span class="survey-stat-box-desc">{{ item.val }}개</span>
+                      <!--                      <span class="survey-stat-box-more">자세히 보기</span>-->
                     </div>
                   </v-col>
                 </v-row>
                 <v-container>
-                  <v-row v-for="(item, i) in surveyQs" :key="i">
+                  <v-row v-for="(item, i) in pollData.slice(0, 4)" :key="i">
                     <v-col class="my-1 pa-2 elevation-1" cols="12" style="border: 1px solid black; border-radius: 10px;">
                       <div>
                         <span class="survey-q ma-auto">
-                          {{ i+1 }}. {{ item }}
+                          {{ i+1 }}. {{ item.subject }}
                         </span>
                       </div>
                     </v-col>
                   </v-row>
+                  <v-expand-transition>
+                    <v-row v-for="(item, i) in pollData.slice(4)" v-show="visible" :key="i">
+                      <v-col class="my-1 pa-2 elevation-1" cols="12" style="border: 1px solid black; border-radius: 10px;">
+                        <div>
+                          <span class="survey-q ma-auto">
+                            {{ i+5 }}. {{ item.subject }}
+                          </span>
+                        </div>
+                      </v-col>
+                    </v-row>
+                  </v-expand-transition>
                   <v-row>
-                    <v-col class="mt-2 pa-0" cols="12">
-                      <div class="survey-q-more">
-                        <div class="survey-more-text">
-                          <span style="font-size: 11px; font-weight: 600;">설문 항목 전체 보기</span>
-                        </div>
-                        <div class="survey-more-btn ml-auto">
-                          <v-icon>mdi-chevron-right</v-icon>
-                        </div>
-                      </div>
+                    <v-col cols="12" class="pa-0">
+                      <v-card-actions class="pa-0 mt-5" style="width: 100%;">
+                        <button class="survey-q-more" style="width: 100%;" @click="visible=!visible">
+                          <div class="survey-more-text">
+                            <span>
+                              설문 항목 전체 보기
+                            </span>
+                          </div>
+                          <div class="survey-more-btn ml-auto">
+                            <v-icon>mdi-chevron-right</v-icon>
+                          </div>
+                        </button>
+                      </v-card-actions>
                     </v-col>
                   </v-row>
                 </v-container>
+              </v-tab-item>
+              <v-tab-item class="pa-2 py-5">
+                <v-row style="padding: 5px;">
+                  <v-col v-if="surveyData.rewardsData" class="" cols="12">
+                    <v-row>
+                      <v-col cols="12">
+                        <span class="survey-rewards-title">이 설문에는 총 {{ surveyData.rewardsData.length }}개의 보상이 있습니다!</span>
+                      </v-col>
+                    </v-row>
+                    <v-row v-for="(item, i) in surveyData.rewardsData" :key="i" class="survey-rewards-box mb-3">
+                      <v-col v-if="item.type === 'point'" cols="12" style="display: flex; align-items: center">
+                        <v-icon color="yellow darken-3" class="mr-2">
+                          mdi-circle-multiple
+                        </v-icon>
+                        {{ item.value | comma }} 포인트
+                      </v-col>
+                      <v-col v-if="item.type === 'giftcon'" cols="12" style="display: flex; align-items: center">
+                        <v-icon color="red" class="mr-2">
+                          mdi-gift
+                        </v-icon>
+                        {{ item.value }} 기프티콘
+                      </v-col>
+                    </v-row>
+                  </v-col>
+                </v-row>
               </v-tab-item>
             </v-tabs-items>
           </v-col>
@@ -194,6 +234,7 @@
 import axios from 'axios'
 import Report from '../../layouts/report.vue'
 import Banner from '../../components/Banner.vue'
+
 export default {
   components: { Banner, Report },
   layout: 'empty',
@@ -201,29 +242,64 @@ export default {
     return {
       tab: null,
       tabs: ['질문', '보상'],
-      more: false,
+      survVisible: false,
+      visible: false,
       surveyData: [],
-      surveyStats: [{ questions: 34 }],
-      surveyQs: ['대면 전환 후 가장 기대되는 활동은?', '학교 행정 편의 만족도는?', '현재 학식 가격에 대한 만족도는?', '가장 필요하다고 생각하는 개선점은?'],
+      surveyContent: '',
+      surveyContentArr: [],
+      pollData: [],
+      surveyStats: [],
       bannerData: [{ icon: 'mdi-vote', title: '임시 배너', msg: '임시 메시지', to: '/' }]
     }
   },
   computed: {
-    showToggle () {
-      const content = this.surveyData.content
-      return String(content).length > 100
-    }
   },
   created () {
     this.fetchData(this.$route.query.id)
+    this.fetchPollData(this.$route.query.id)
   },
   methods: {
     fetchData (id) {
       axios.get(
         'https://api.govey.app/users/v1/surveys/' +
           id
-      ).then((response) => {
+      ).then(async (response) => {
         this.surveyData = response.data
+        this.surveyContent = response.data.content
+        this.surveyContentArr.push(this.surveyContent)
+        const rewardsData = await axios.get(
+          'https://api.govey.app/users/v1/surveys/' +
+              id +
+              '/rewards/'
+        ).then((response) => {
+          const reward = []
+          for (let i = 0; i < response.data.length; i++) {
+            const dic = {}
+            dic.type = response.data[i].type
+            dic.value = response.data[i].value
+            reward.push(dic)
+          }
+          return reward
+        }).catch((error) => {
+          console.log(error)
+        })
+        const mix = Object.assign({}, this.surveyData, { rewardsData })
+        this.surveyData = mix
+      }).catch((error) => {
+        console.log(error)
+      })
+    },
+    fetchPollData (id) {
+      axios.get(
+        'https://api.govey.app/users/v1/surveys/' +
+          id +
+          '/polls/'
+      ).then((response) => {
+        this.pollData = response.data
+        const arr = {}
+        arr.type = '설문 항목'
+        arr.val = this.pollData.length
+        this.surveyStats.push(arr)
       }).catch((error) => {
         console.log(error)
       })
@@ -249,7 +325,7 @@ export default {
 }
 .survey-more {
   display: flex;
-  flex-display: column;
+  align-items: center;
   vertical-align: center;
   width: 100%;
   margin: 20px 0;
@@ -260,6 +336,8 @@ export default {
 .survey-more-text {
   display: flex;
   vertical-align: center;
+  font-size: 12px;
+  font-weight: 600;
 }
 .part-btn {
   display: flex;
@@ -287,17 +365,15 @@ export default {
   font-size: 12px;
 }
 .survey-stat-box-desc {
+  display: flex;
+  align-items: center;
   font-size: 14px;
   font-weight: 600;
-}
-.survey-stat-box-more {
-  padding-top: 3px;
-  font-size: 10px;
 }
 .survey-q {
   padding: 5px;
   color: black;
-  font-size: 12px;
+  font-size: 14px;
   font-weight: 500;
 }
 .survey-q-more {
@@ -306,5 +382,17 @@ export default {
   align-items: center;
   background-color: #eeeeee;
   border-radius: 10px;
+}
+.survey-rewards-title {
+  margin: 5px 0;
+}
+.survey-rewards-box {
+  display: flex;
+  flex-direction: column;
+  padding: 3px;
+  background-color: #ececec;
+  border-radius: 10px;
+  font-size: 14px;
+  font-weight: 600;
 }
 </style>
